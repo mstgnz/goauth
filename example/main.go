21b37db351c98a04e80cdc8e4fef06688b7fed3d
Mesut GENEZ <genezmesut@gmail.com>
2024-12-30 23:13:58 +0300

    example

--- example/main.go
+++ example/main.go
@@ -4,21 +4,21 @@ import (
 	"log"
 	"net/http"
 
-	config "github.com/mstgnz/goauth/config"
-	"github.com/mstgnz/goauth/providers"
+	"github.com/mstgnz/goauth/initialize"
+	"github.com/mstgnz/goauth/provider"
 	"golang.org/x/oauth2"
 )
 
 var err error
-var provider config.Provider
+var provide provider.Provider
 
 func main() {
 
-	provider, err = goauth.NewProviderByName("github")
+	provide, err := initialize.NewProviderByName("github")
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	provider.SetRedirectUrl("http://localhost:8585/callback")
+	provide.SetRedirectUrl("http://localhost:8585/callback")
 
 	http.HandleFunc("/login", handleLogin)
 	http.HandleFunc("/callback", handleCallback)
@@ -28,16 +28,16 @@ func main() {
 }
 
 func handleLogin(w http.ResponseWriter, r *http.Request) {
-	url := provider.BuildAuthUrl("state", oauth2.AccessTypeOffline, oauth2.ApprovalForce)
+	url := provide.BuildAuthUrl("state", oauth2.AccessTypeOffline, oauth2.ApprovalForce)
 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
 }
 
 func handleCallback(w http.ResponseWriter, r *http.Request) {
-	token, err := provider.FetchToken(r.URL.Query().Get("code"))
+	token, err := provide.FetchToken(r.URL.Query().Get("code"))
 	if err != nil {
 		log.Println(err)
 	}
-	if user, err := provider.FetchUser(token); err != nil {
+	if user, err := provide.FetchUser(token); err != nil {
 		log.Println(err)
 	} else {
 		log.Println(user)
